63c3d5b79c5404c37c116c4f07422334b000d03e
Ricardo <ricardomedeirosx@gmail.com>
2025-07-25 15:12:14 +0100

    PIPEX -V.01 (remove comments, add errors, try to clean the code and add ft_split)

--- pipe_process.c
+++ pipe_process.c
@@ -6,28 +6,11 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 11:19:06 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/07/23 12:37:06 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/07/25 15:11:20 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
-
-
-/*
-As said before, inside the pipe, everything goes to one of its ends, one end will write and the other will read.
-end[1] is the child process, and end[0] the parent process: the child writes, while the parent reads. 
-And since for something to be read, it must be written first, so cmd1 will be executed by the child, 
-and cmd2 by the parent.
-
-For the child process, we want infile to be our stdin (we want it as input), and end[1] to be our
-stdout (we want to write to end[1] the output of cmd1).
-In the parent process, we want end[0] to be our stdin (end[0] reads from end[1] the output of cmd1), 
-and outfile to be our stdout (we want to write to it the output of cmd2).
-
-dup2() swaps our files with stdin and stdout. dup2() can swap our fds to stdin/stdout.
-It will close fd2 and duplicate the value of fd2 to fd1 (it will redirect fd1 to fd2).
-
-*/
 
 void cmd1_process(int pipefd[2], int input_fd, char *cmd, char **envp)
 {
@@ -39,40 +22,6 @@ void cmd1_process(int pipefd[2], int input_fd, char *cmd, char **envp)
     execute_command(cmd, envp); // execute the command (cmd)
 }
 
-/*
-child_process(f1, cmd1)
-
-dup2 close stdin, f1 becomes the new stdin
-dup2(f1, STDIN_FILENO); // we want f1 to be execve() input
-dup2(end[1], STDOUT_FILENO); // we want end[1] to be execve() stdout
-close(end[0]) --> always close the end of the pipe we don't use,
-                    as long as the pipe is open, the other end will 
-                    be waiting for some kind of input and will not
-                    be able to finish its process
-close(f1)
-// execve function for each possible path
-exit(1); // exit the child process if execve fails
-*/
-
-/*
-e.g: shell infile < grep hello |  wc -l > outfile
-         ./pipex infile "grep hello" "wc -l" outfile
-
-1) dup2 (infile, STDIN_FILENO) -> redirect the stdin (descriptor 0) to the infile. STDIN (0) -> infile
-   Thus grep hello will read the content from infile.
-
-2) dup2 (pipefd[1], STDOUT_FILENO) -> redirect the stdout (descriptor 1) to the pipe's write end (pipefd[1]).
-   Replace stdout (1) with pipefd[1] (write end of pipe). STDOUT (1) -> pipefd[1] 
-   Thus grep hello will write its output to the pipe.
-   Everything that grep hello prints will be sent to the pipe, not to the terminal.
-
-3) close(pipefd[0]) -> close pipefd[0] (read end of pipe), because cmd1 only writes.
-
-4) execute_cmd(cmd, envp) -> execute the command (cmd) with the environment variables (envp).
-   Thus grep hello will be executed with its stdin coming from infile and the stdout going to the pipe.
-
-*/
-
 void cmd2_process(int pipefd[2], int output_fd, char *cmd, char **envp)
 {
     dup2(pipefd[0], STDIN_FILENO); // redirect input from pipe (pipefd[0]) -> stdin: pipefd[0] (read from pipe)
@@ -83,112 +32,35 @@ void cmd2_process(int pipefd[2], int output_fd, char *cmd, char **envp)
     execute_command(cmd, envp); // execute the command (cmd)
 }
 
-/*
-parent_process(f2, cmd2);
-
-dup2(f2, ...); // f2 is the stdout
-dup2(end[0], ...); // end[0] is the stdin
-close(end[1])
-close(f2);
-// execve function for each possible path
-exit(1); // exit the parent process if execve fails
-*/
-
-/*
-
-1) dup2 (pipefd[0], STDIN_FILENO) -> redirect the stdin (descriptor 0) to the pipe's read end (pipefd[0]).
-   Thus wc -l will read the content from the pipe (what grep hello wrote to the pipe).
-
-2) dup2 (output_fd, STDOUT_FILENO) -> redirect the stdout (descriptor 1) to the file2 (output_fd).
-   Replace stdout (1) with output_fd (write end of file2). STDOUT (1) -> output_fd
-   Thus wc -l will write its output to file2.
-
-3) close(pipefd[0]) -> close pipefd[0] (read end of pipe), because cmd2 only reads.
-
-4) execute_cmd(cmd, envp) -> execute the command (cmd) with the environment variables (envp).
-   Thus wc -l will be executed with its stdin coming from the pipe and the stdout going to file2.
-   Execute the wc -l  with the redirected stdin and stdout: 
-    - reads from the pipe (pipefd[0]) what grep hello wrote to the pipe
-    - writes the output to file2 (output_fd).
-*/
-
-void pipe_process(int pipe_fd[2], int fd1, int fd2, char **av, char **envp)
+void	pipe_process(int p_fd[2], int fd1, int fd2, char **av, char **envp)
 {
-    pid_t pid1;
-    pid_t pid2;
+	pid_t	pid1;
+	pid_t	pid2;
 
-    pid1 = fork(); // create a child process for cmd1
-    if (pid1 == -1)
-    {
-        error . . .
-        exit(1);
-    }
-    if (pid1 == 0)
-    {
-        cmd1_process(pipe_fd, fd1, av[2], envp); // child process for cmd1
-        exit(1);
-    }
-    pid2 = fork(); // create a child process for cmd2
-    if (pid2 == -1)
-    {
-        error . . .
-        exit(1);
-    }
-    if (pid2 == 0)
-    {
-        cmd2_process(pipe_fd, fd2, av[3], envp); // child process for cmd2
-        exit(1);
-    }
-    close (pipe_fd[0]); // close read end of pipe in parent process
-    close (pipe_fd[1]); // close write end of pipe in parent process
-    waitpid(pid1, NULL, 0); // wait for cmd1 to finish
-    waitpid(pid2, NULL, 0); // wait for cmd2 to finish
+	pid1 = fork();
+	if (pid1 == -1)
+	{
+		perror("fork 1 fail");
+		exit(1);
+	}
+	if (pid1 == 0)
+	{
+		cmd1_process(p_fd, fd1, av[2], envp);
+		exit(1);
+	}
+	pid2 = fork();
+	if (pid2 == -1)
+	{
+		perror("fork 2 fail");
+		exit(1);
+	}
+	if (pid2 == 0)
+	{
+		cmd2_process(p_fd, fd2, av[3], envp);
+		exit(1);
+	}
+	close(p_fd[0]);
+	close(p_fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 }
-
-/*
-The fork() will split our process in two sub-processes: it returns 0 for the child 
-process, a non-zero number for the parent process, or a -1 in case of error.
-Also... fork() splits the process in two parallel, simultaneous processes, that happen at 
-the same time.
-
-The fork() runs two processes (i.e. two commands) in one single program; 
-
-With waitpid() at the very beginning to wait for the child to finish her process.
-With the right stdin and stdout, we can execute the command with execve() 
-*/
-
-/*
-cmd1 ("grep hello"):
-  stdin  <- infile
-  stdout -> pipefd[1]
-
-cmd2 ("wc -l"):
-  stdin  <- pipefd[0]
-  stdout -> outfile
---------------------------------------------------------------
-         infile.txt
-     ╔═════════════════════╗
-     ║    hello world      ║
-     ║  this is a  infile  ║
-     ║     hello again     ║
-     ║    no match here    ║
-     ╚═════════════════════╝
-            ▼
-       "grep hello" (cmd1)
-            ▼
-      ╔═════════════╗
-      ║    PIPE     ║
-      ║ hello world ║  ← there only pass the text with "hello"
-      ║ hello again ║
-      ╚═════════════╝
-            ▼
-         "wc -l" (cmd2)
-            ▼
-        outfile.txt
-         ╔═════════╗
-         ║    2    ║
-         ╚═════════╝
-----------------------------------------------------------------
-
-*/
-
--- pipex.c
+++ pipex.c
@@ -6,67 +6,9 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 11:08:18 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/07/23 13:01:51 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/07/25 15:10:48 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// ./pipex infile cmd1 cmd2 outfile
-
-/*
-The general idea: we read from infile, execute cmd1 with infile as input, send 
-the output to cmd2, which will write to outfile.
-The pipe() sends the output of the first execve() as input to the second execve();
-
-Each command needs a stdin (input) and returns an output (to stdout).
-
-    infile                                             outfile
-as stdin for cmd1                                 as stdout for cmd2            
-       |                        PIPE                        ↑
-       |           |---------------------------|            |
-       ↓           |                           |            |
-      cmd1   -->  end[1]       ↔             end[0]   -->  cmd2           
-                   |                           |
-      cmd1         |---------------------------|           end[0]
-      output                                               reads end[1]
-      is written                                           and sends cmd1
-      to end[1]                                            output to cmd2
-      (end[1] becomes                                      (end[0] becomes 
-      cmd1 stdout)                                         cmd2 stdin)
-
-
-The pipe() takes an array of two integers such as int end[2], and links them together. 
-In a pipe, what is done in end[0] is visible to end[1], and vice versa. 
-Plus, pipe() assigns an fd to each end.
-Fd are file descriptors, and since files can be read and written to, by getting an fd each, 
-the two ends can communicate: end[1] will write to the its own fd, and end[0] will read end[1]’s
-fd and write to its own.
-
-Inside the pipe, everything goes to one of its ends, one end will write and the other will read.
-end[1] is the child process, and end[0] the parent process: the child writes, while the parent reads. 
-And since for something to be read, it must be written first, so cmd1 will be executed by the child, 
-and cmd2 by the parent.
-
-Pipe creates fds. We run pipex like this ./pipex infile cmd1 cmd2 outfile , so infile and outfile need 
-to become the stdin and stdout of the pipe.
-
-Our fd table right now looks like this:
-
-                           -----------------    
-                 0         |     stdin     |  
-                           -----------------    
-                 1         |     stdout    |    
-                           -----------------    
-                 2         |     stderr    |  
-                           -----------------
-                 3         |     infile    |  // open()
-                           -----------------
-                 4         |     outfile   |  // open()
-                           -----------------
-                 5         |     end[0]    | 
-                           -----------------
-                 6         |     end[1]    |  
-                           -----------------
-*/
 
 int	main(int ac, char **av, char **envp)
 {
