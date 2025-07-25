a7748d1a4340c2d4df72ab12399fb2e1617f71a1
Ricardo <ricardomedeirosx@gmail.com>
2025-07-25 16:13:44 +0100

    PIPEX V0.2 (modify AGAIN the child and parent processes to improve the main function and deal with the errors)

--- pipe_process.c
+++ pipe_process.c
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 11:19:06 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/07/25 15:44:23 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/07/25 16:11:00 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+
+void	free_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	if (!tab)
+		return ;
+	while (tab[i])
+		free(tab[i++]);
+	free(tab);
+}
+
+char *find_path(char *cmd, char** envp)
+{
+    int i;
+    char *exec_path;
+    char **allpath;
+    char *path_part;
+    char **args;
+
+    i = -1;
+    allpath = ft_split(getenv("PATH", envp), ':'); // split PATH environment variable into directories
+    
+}
 
 void execute_command(char *cmd, char **envp)
 {
@@ -31,39 +56,49 @@ void execute_command(char *cmd, char **envp)
 	{
 		ft_putstr_fd("command not found: ", 2);
 		ft_putendl_fd(args[0], 2);
-		free_split(args);
+		free_tab(args);
 		exit(127);
 	}
     exceve(path, args, envp); // execute the command with the environment variables
     perror("execve failed"); // if execve fails, print error message
-    free_split(args); // free the arguments
+    free_tab(args); // free the arguments
     free(path);
     exit(1); // exit if execve fails
 }
 
 void cmd1_process(char **av, int pipefd[2], char **envp)
 {
-    int fd;
+    int infile;
 
-    fd = open(av[1], O_RDONLY); // open file1 (av[1]) for reading
-    dup2(fd, STDIN_FILENO); // redirect input from file1 (fd) -> stdin: fd ()
+    infile = open(av[1], O_RDONLY, 0444); // open file1 (av[1]) for reading
+    if (infile == -1)
+    {
+        close (pipefd[1]);
+        ft_putstr_fd("error opening file1", 2);
+        exit(1);
+    }
+    dup2(infile, STDIN_FILENO); // redirect input from file1 (infile) -> stdin: infile ()
     dup2(pipefd[1], STDOUT_FILENO); // redirect output to pipe (pipefd[1]) -> stdout: pipefd[1] (write on pipe)
     close(pipefd[0]); // close read end of pipe (pipefd[0])
-    close(pipefd[1]); // cclose write end of pipe (pipefd[1])
-    close(fd); // close file descriptor for file1 (close input_fd)
+    close(infile); // close file descriptor for file1 (close input_fd)
     execute_command(av[2], envp); // execute the command (cmd)
 }
 
 void cmd2_process(char **av, int pipefd[2], char **envp)
 {
-    int fd;
+    int outfile;
 
-    fd = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644); // open file2 (av[4]) for writing
+    outfile = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644); // open file2 (av[4]) for writing
+    if (outfile == -1)
+    {
+        close(pipefd[0]);
+        ft_putstr_fd("error opening file2", 2);
+        exit(1);
+    }
     dup2(pipefd[0], STDIN_FILENO); // redirect input from pipe (pipefd[0]) -> stdin: pipefd[0] (read from pipe)
-    dup2(fd, STDOUT_FILENO); // redirect output to file2 (fd) -> stdout: fd (write on file2)
-    close(pipefd[0]); // close read end of pipe (pipefd[0])
+    dup2(outfile, STDOUT_FILENO); // redirect output to file2 (outfile) -> stdout: outfile (write on file2)
     close(pipefd[1]); // close write end of pipe (pipefd[1])
-    close(fd); // close file descriptor for file2 (close output_fd)
+    close(outfile); // close file descriptor for file2 (close output_fd)
     execute_command(av[3], envp); // execute the command (cmd)
 }
 
@@ -107,24 +142,33 @@ int main(int ac, char **av, char **envp)
     int pipefd[2];
     pid_t pid;
 
-    if (ac != 5)
+    if (!envp)
     {
-        write(2, "Usage: ./pipex file1 cmd1 cmd2 file2\n", 37);
-        exit (1);
+        ft_putstr_fd("no environment variablesfound\n", 2);
+        exit(1);
     }
-    if (pipe(pipefd) == -1)
+    if (ac == 5)
     {
-        perror("...");
-        exit (1);
+       if (pipe(pipefd) == -1)
+        {
+            perror("failed to create pipe");
+            exit (1);
+        }
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork failed");
+            exit (1);
+        }
+        else if (pid == 0)
+            cmd1_process(av, pipefd, envp);
+        else
+            cmd2_process(av, pipefd, envp);
     }
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("...");
-        exit (1);
-    }
-    if (!pid)
-        cmd1_process(av, pipefd, envp);
     else
-        cmd2_process(av, pipefd, envp);
+    {
+        ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 file2\n", 2);
+        exit(1);
+    }
+    return (0);
 }
