faf3e7708624b65dfb7fd5dde1242b9b60c62b43
Ricardo <ricardomedeirosx@gmail.com>
2025-07-30 22:31:37 +0100

    PIPEX V2.3 (create a new function to parse the cmd with ft_split_quotes and add in ft_exec_cmd function)

--- exec_cmd.c
+++ exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 14:37:52 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/07/29 17:16:30 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/07/30 22:29:05 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,19 @@ static void check_cmd_access(char *cmd, char **args)
     }
 }
 
+static char **ft_parse_cmd(char *cmd)
+{
+    char **cmd_list;
+
+    cmd_list = ft_split_quotes(cmd, ' ');
+    if (!cmd_list || !cmd_list[0])
+    {
+        ft_free_str(cmd_list);
+        cmd_notfound(cmd);
+    }
+    return (cmd_list);
+}
+
 void ft_exec_cmd(char *cmd, char **envp)
 {
     char **cmd_list;
@@ -33,12 +46,7 @@ void ft_exec_cmd(char *cmd, char **envp)
 
     if (!cmd || cmd[0] == '\0')
         cmd_notfound(cmd);
-    cmd_list = ft_split_quotes(cmd, ' ');
-    if (!cmd_list || !cmd_list[0])
-    {
-        ft_free_str(cmd_list);
-        cmd_notfound(cmd);
-	}
+    cmd_list = ft_parse_cmd(cmd);
     if (ft_strchr(cmd_list[0], '/'))
 		check_cmd_access(cmd, cmd_list);
     cmd_path = ft_cmd_path(cmd_list[0], envp);
